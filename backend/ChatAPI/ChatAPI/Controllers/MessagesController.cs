b72e62c2ee94880d43a93a284147c84f1ec6ffc9
Okan Alat <86848048+okanalatt@users.noreply.github.com>
2025-10-25 12:21:46 +0300

    Add files via upload

--- backend/ChatAPI/ChatAPI/Controllers/MessagesController.cs
+++ backend/ChatAPI/ChatAPI/Controllers/MessagesController.cs
@@ -1,7 +1,13 @@
-﻿using ChatAPI.Data;
-using ChatAPI.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Text.Json;
+using ChatAPI.Data;
+using ChatAPI.Models;
+using System.Net.Http.Headers;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+
+
 
 namespace ChatAPI.Controllers
 {
@@ -10,32 +16,62 @@ namespace ChatAPI.Controllers
     public class MessagesController : ControllerBase
     {
         private readonly AppDbContext _context;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
 
-        public MessagesController(AppDbContext context)
+        public MessagesController(AppDbContext context, IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
             _context = context;
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
         }
 
-        //Get - api/Messages
+        // Get - api/Messages
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Message>>> GetMessages()
         {
             return await _context.Messages.OrderBy(m => m.Timestamp).ToListAsync();
         }
 
-        //Post - api/Messages
+        // Post - api/Messages
         [HttpPost]
         public async Task<ActionResult<Message>> PostMessage(Message message)
         {
-            message.Timestamp = DateTime.Now;
-            _context.Messages.Add(message);
+            string apiKey = _configuration.GetValue<string>("AIServices:ApiKey");
 
-            await _context.SaveChangesAsync();
+            string model= _configuration.GetValue<string>("AIServices:Model");
 
-            return CreatedAtAction(nameof(GetMessages), new
+            string url = $"https://api-inference.huggingface.co/models/{model}";
+
+            // HttpClient oluştur
+            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+            // Analiz edilecek cümle
+            var inputText = message.Description;
+
+            var requestBody = new
             {
-                id = message.Id
-            }, message);
+                inputs = inputText
+            };
+
+            string json = JsonSerializer.Serialize(requestBody);
+            using var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync(url, content);
+
+            string result = await response.Content.ReadAsStringAsync();
+
+             var sentimentResponse = JsonSerializer.Deserialize<List<List<SentimentResponse>>>(result);
+
+            //var dominantSentiment = sentimentResponse
+            //.OrderByDescending(r => r.Max(x=>x.score)) // Skora göre büyükten küçüğe sırala
+            //.FirstOrDefault();              // İlk elemanı (en büyük skora sahip olanı) al
+
+            //var r = dominantSentiment.First();
+          var resultSentiment = sentimentResponse.First().OrderByDescending(x=>x.score).First();
+
+            return Ok(resultSentiment);
         }
     }
 }
