e2a3b2874896dcf9a6d944dca0928d63b67f8f2b
Okan Alat <86848048+okanalatt@users.noreply.github.com>
2025-10-25 13:44:53 +0300

    Add files via upload

--- backend/ChatAPI/ChatAPI/Data/AppDbContext.cs
+++ backend/ChatAPI/ChatAPI/Data/AppDbContext.cs
@@ -9,6 +9,14 @@ namespace ChatAPI.Data
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // EF Core'a bu sınıfı veritabanına kaydetmesini istemediğimizi söylüyoruz.
+            modelBuilder.Entity<SentimentResponse>().HasNoKey();
+
+            base.OnModelCreating(modelBuilder);
+        }
         public DbSet<Message>Messages { get; set; }
+        public DbSet<SentimentResponse> SentimentResponses { get; set; }
     }
 }
